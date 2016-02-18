684e22a024433bd05053c1783e8d2eaf94fe2ea5
Vitalii Kotliar <vitaliikotliar1@gmail.com>
2016-02-18 14:52:18 +0200

    generat dom matrix

--- client/js/partials/createDOMSudoku.js
+++ client/js/partials/createDOMSudoku.js
@@ -5,6 +5,21 @@
  */
 
 function createDOMSudoku(matrixStart) {
-    console.log(matrixStart);
+    var $container = $('#container-sudoku');
+    var htmlTmp = "";
 
+    var length = matrixStart.length;
+    for (var i = 0; i < length; i++) {
+        htmlTmp = htmlTmp + '<div class="board__row g-clearfix" id="row'+ i +'">'
+        var lengthInner = matrixStart.length;
+        for (var j = 0; j < lengthInner; j++) {
+
+
+            htmlTmp = htmlTmp + '<div class="board__cell">'+ matrixStart[i][j] +'</div>';
+        }
+        htmlTmp = htmlTmp + '</div>';
+
+    }
+    var message = $(htmlTmp);
+    $container.append(message);
 };
